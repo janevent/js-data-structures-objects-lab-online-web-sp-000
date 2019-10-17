86ea017d300d3817b13d60a8bc69cd140f2ad1a8
Jane Venter <janeanneventer@gmail.com>
2019-10-17 19:01:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromDriverByKey(obj, key){
-  let newObj = Object.assign({}, object);
+  let newObj = Object.assign({}, obj);
   delete newObj[key];
   return newObj;
 }
